617a684226933a4ddad55d093eb524c49fe0dc51
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-20 15:18:11 -0700

    Write main function to run MCS

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -5,7 +5,7 @@ public class PercolationStats {
     private Percolation percTestRun;
 
     private int trials; // store number of trials
-    private int[] percThreshold; // will store percolation threshold for each run
+    private double[] percThreshold; // will store percolation threshold for each run
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials){
@@ -16,11 +16,11 @@ public class PercolationStats {
             throw new  IndexOutOfBoundsException("Trials must be > 0");
 
         this.trials = trials;
-        percThreshold = new int[trials]; // one spot for each trial
+        percThreshold = new double[trials]; // one spot for each trial
         int rowToOpen;  // random row to open
         int colToOpen;  // random column to open
 
-        for (int currTrial = 1; currTrial <= trials ; currTrial++) {
+        for (int currTrial = 0; currTrial < trials ; currTrial++) {
             percTestRun = new Percolation(n); // new percolation object
 
             while(!percTestRun.percolates()){
@@ -29,10 +29,10 @@ public class PercolationStats {
                 percTestRun.open(rowToOpen,colToOpen);
             }
 
-            percThreshold[currTrial] = percTestRun.numberOfOpenSites(); // save threshold to array
+            percThreshold[currTrial] = (double) percTestRun.numberOfOpenSites()/(n*n); // save threshold (site vacancy ration) to array
 
         }
-        System.out.print("Monte Carlo simulation complete with " + trials + " on a " + n + "x" + n + " grid.");
+        System.out.println("Monte Carlo simulation complete with " + trials + " trials on a " + n + "x" + n + " grid.\n");
     }
 
     // sample mean of percolation threshold
@@ -57,6 +57,22 @@ public class PercolationStats {
 
     // test client (see below)
     public static void main(String[] args){
-        
+        // Check for valid arguments
+        if(args.length < 2){
+            System.err.println("Requires two argument: <grid size> <trials>");
+            System.exit(1);
+        }
+
+        // parse command line arguments
+        int gridSizeN = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        // Run Monte Carlo sim
+        PercolationStats percStats = new PercolationStats(gridSizeN, trials);
+
+        // print results
+        System.out.println("mean \t\t\t\t\t= " + percStats.mean());
+        System.out.println("stddev \t\t\t\t\t= " + percStats.stddev());
+        System.out.println("95% confidence interval = [" + percStats.confidenceLo() + ", " + percStats.confidenceHi() + "]");
     }
 }
