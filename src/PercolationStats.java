55dcf714be92028e5b35d641817cc2ae8a4ec99f
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-20 14:24:30 -0700

    Create constructor for PercolationStats

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,10 +1,41 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+    private Percolation percTestRun;
+
+    private int[] percThreshold; // percolation threshold for each run
 
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials)
+    public PercolationStats(int n, int trials){
+        //Check for valid inputs
+        if(n <= 0)
+            throw new  IndexOutOfBoundsException("Grid size must be > 0");
+        if(trials <= 0)
+            throw new  IndexOutOfBoundsException("Trials must be > 0");
+
+        percThreshold = new int[trials]; // one spot for each trial
+        int rowToOpen;
+        int colToOpen;
+
+        for (int currTrial = 1; currTrial <= trials ; currTrial++) {
+            percTestRun = new Percolation(n); // new percolation object
+
+            while(!percTestRun.percolates()){
+                rowToOpen = StdRandom.uniform(1,n+1);   // random num in interval [a;b)
+                colToOpen = StdRandom.uniform(1, n+1);
+                percTestRun.open(rowToOpen,colToOpen);
+            }
+
+            percThreshold[currTrial] = percTestRun.numberOfOpenSites(); // save threshold to array
+
+        }
+        System.out.print("Monte Carlo simulation complete with " + trials + " on a " + n + "x" + n + " grid.");
+    }
 
     // sample mean of percolation threshold
-    public double mean()
+    public double mean(){
+    }
 
     // sample standard deviation of percolation threshold
     public double stddev()
