8a82dc316bb7fa4741b393c53bccb659cd00233f
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-20 08:25:18 -0700

    Add constructor and class fields

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,7 +1,20 @@
 public class Percolation {
+    private boolean[][] grid;
+    private boolean percolates;
+    private int[] roots;
+    private int n;
+    private int numOpenSites = 0;
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n)
+    public Percolation(int n){
+        this.n = n; // save value for n*n grid
+        grid = new boolean[n][n];   // all false (blocked) by default
+        roots = new int[n*n];       // n*n array for roots
+        // initialize roots[] array so that each square is its own root
+        for(int i = 0; i < n*n; i++){
+            roots[i] = i;
+        }
+    }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col)
