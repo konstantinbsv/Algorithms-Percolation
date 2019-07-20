94590126f6b3fb40ad810fd23df73236c71cde45
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-20 12:09:42 -0700

    New open(int,int) method: checks for corner cases and unions with any open neighbouring sites

--- src/Percolation.java
+++ src/Percolation.java
@@ -45,6 +45,27 @@ public class Percolation {
     public void open(int row, int col){
         throwIfNotValid(row, col);
 
+        /* First and Last rows */
+        if(row == 1) // if site to open is on first row
+            wquf.union(xyTo1D(row,col), 0); // union with top virtual site
+        else
+            if(isOpen(row-1, col)) // if site on previous is open
+                wquf.union(xyTo1D(row,col), xyTo1D(row-1,col));
+
+        if(row == n) // if site to open is on last row
+            wquf.union(xyTo1D(row,col), n*n+1); // union with bottom virtual site
+        else
+            if(isOpen(row+1, col)) // if site on next row is open
+                wquf.union(xyTo1D(row,col), xyTo1D(row+1,col));
+
+        /* Left and Right side columns */
+        if(col > 1) // if site is not on left-most column
+            if(isOpen(row, col-1)) // if previous site on same row is open
+                wquf.union(xyTo1D(row,col), xyTo1D(row,col-1));
+
+        if(col < n) // if site is not on right-most column
+            if(isOpen(row, col+1)) // if next site on same row is open
+                wquf.union(xyTo1D(row,col), xyTo1D(row,col+1));
 
 
         grid[row - 1][col - 1] = true;  //Set this site as open in boolean grid
